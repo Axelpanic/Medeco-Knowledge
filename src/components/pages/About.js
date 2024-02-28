8cb436eb39bd4bb534e3b36be53fdddc7134b9da
Alex Hickey <112765530+Axelpanic@users.noreply.github.com>
2024-02-28 05:26:11 +0000

    formatting about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,19 +8,21 @@ export default function About() {
       <div className='cardish'>
         <p> 
           Roy C. Spain had a idea. In 1955 he stared his own buislness in his own basement in Roanoke, Virigina. He previously worked for Yale (yes, that yale). He brought on three other folks, Fred Wagner, Elvis Flora, and ROy Oliver. 
-
+</p>
+<p>
           They started the Mechanical Development Company, or MEDECO. In 1957, they had their first prototype of the key and decided not to push forward. But on March 1st 1967, Roy Spain submitted a application to the Patent office using his and his three coworkers as the designers.
-
+</p>
+<p>
           (patent image one)
-
+</p><p>
           None of them wanted to be locksmiths or lock designers, but they made a lock that was very resistant to picking which was something NYC needed at the time. In the early 70's, Medeco put out a contest for $10,000 cash to anyone who could pick open their new lock. In 1972, one NYC police detective was able to open one lock but was not able to do it again. He won $1,500.
-
+</p><p>
           This contest brought MEDECO into the spotlight. They sold very well for a while, especially in NYC.
-
+</p><p>
           In 1972, Roy C Spain sold his rights to the patent to Roy Oliver and Oliver became the head of MEDECO. After the intial popularity died down, they were struggling to stay afloat. In 1983, Hillenbrand Industries bought MEDECO. They expanded MEDECO and introducted the MEDECO Biaxial around the world!
-          
+          </p><p>          
           In the mid 1990's, MEDECO kept up R&D to be on the leading edge of lock manufacturing. In 1996 MEDECO changed and sold directly to retail locksmiths, saving locksmiths money and creating profits for MEDECO as well!
-
+          </p><p>
           In 1998, MEDECO was sold again. This time, they were sold to ASSA ABLOY Group. They now had partners around the world and more tools than they knew what to do with. Using ASSA ABLOY group's patents, they created the M3 soon after in 2005 and the M4 in 2017! Both are UL patented to gurantee key making security.
         </p>
       </div>
