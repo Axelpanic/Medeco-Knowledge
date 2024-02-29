8854f3fddba206a5745f17b823c09c3bdeaf0d17
Alex Hickey <112765530+Axelpanic@users.noreply.github.com>
2024-02-29 04:35:25 +0000

    editing skills

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -7,7 +7,18 @@ export default function About() {
       <h2>Resources on Picking MEDECO</h2>
       <div className='cardish'>
         <p> 
-            MEDECO has four main variations of locks. These are the ORIGINAL, BIAXIAL, MEDECO3, and MEDECO4
+            Below, You will find several other Lockpicker United members and their information and picking videos on Medeco locks
+            </p>
+            <p>
+              <h4>Artichoke 2000</h4>
+              <iframe width="560" height="315" padding="10" src="https://www.youtube.com/embed/DNpAIZl4krc?si=sUgIzlLa22fUAL8V" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+            </p>
+            <p>
+              <h4>Georgia Jim</h4>
+              <iframe width="560" height="315" padding="10" src="https://www.youtube.com/embed/JTnYYhhBkkQ?si=CVcMPR9s8wcG1y9k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <iframe width="560" height="315" padding="10" src="https://www.youtube.com/embed/OsJure9m4B4?si=bwK8rboO4aojMbcC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
             </p>
       </div>
     </div>
