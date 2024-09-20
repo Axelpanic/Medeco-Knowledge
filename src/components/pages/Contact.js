450bf4562b8e434c1d5c7189983dbbc72c6bc098
Alex Hickey <112765530+Axelpanic@users.noreply.github.com>
2024-09-20 02:23:03 +0000

    adding info to design page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,6 +9,18 @@ export default function About() {
         <p> 
             MEDECO has four main variations of locks. These are the ORIGINAL, BIAXIAL, MEDECO3, and MEDECO4
             </p>
+        <p>
+            <h4> Medeco Classic</h4>
+        </p>
+        <p> 
+          The Medeco Classic comes with a few different driver pin types: Mushroom style and Standard style.
+        </p>
+        <p>
+        <img src={'../../assets/images/patent 1970 blownup.png'} alt={"patent of medeco classic"} />
+        </p>
+        <p> 
+          The 'V' shaped pins make for easy spinning and manipulation as opposed to later versions.
+        </p>
       </div>
     </div>
   );
