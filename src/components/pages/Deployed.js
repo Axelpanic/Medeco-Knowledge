ebbfc3a0af7fb00a67ec83002b78edd269445757
Alex Hickey <112765530+Axelpanic@users.noreply.github.com>
2025-01-16 02:04:04 +0000

    adding clarification to deployed page

--- src/components/pages/Deployed.js
+++ src/components/pages/Deployed.js
@@ -13,10 +13,10 @@ export default function About() {
 
             </p>
             <p> The two main ways to pick the main medeco locks: Clockwise and Counter Clockwise. </p>
-            <p> Clockwise is my favorite method. You attempt to set the rotation of the pins by lifting and push/pull the key pin. Once you are confident in its rotation, push to sheer. Once you have done this with all the keypins and the driver pins are set to sheer, you should fall into a false set. This means one or more pins are in a false gate. Simply lighen tension a little bit and attempt to move the key pins into their true gate and the lock will open once they are fixed! Many say Clock wise is a harder method.
+            <p> Clockwise is my favorite method, it binds the keypins and driver pins first. You attempt to set the rotation of the pins by lifting and push/pull the key pin. Once you are confident in its rotation, push to sheer. Once you have done this with all the keypins and the driver pins are set to sheer, you should fall into a false set. This means one or more pins are in a false gate. Simply lighen tension a little bit and attempt to move the key pins into their true gate and the lock will open once they are fixed! Many say Clock wise is a harder method.
             </p>
             <p> 
-              Counter clockwise is the most popular way to open medecos. This involves setting the rotations to the sidebar first. Once you can add firm tension and all the pins lift without resistance, you are set to sidebar. Add normal tension again and push the pins to sheer. 
+              Counter clockwise is the most popular way to open medecos, this binds the sidebar first. This involves setting the rotations to the sidebar first. Once you can add firm tension and all the pins lift without resistance, you are set to sidebar. Add normal tension again and push the pins to sheer. 
             </p>
             <p> For a great resource, see Lockfall Labs video on the resource tab.</p>
             <p>
