7173a4386f505f19582d7eeaa33a8562380abaf0
Alex Hickey <alexthickey@gmail.com>
2026-03-31 19:35:03 +0000

    remove LPU for ai bull

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,9 +17,11 @@ export default function Home() {
           </p>
         <p>Click on a page above to see various pieces of information.
         </p>
-        <p><img src={Lpu} style={{width:200, height:200, padding:15}} alt="lpu logo" /> <img src={Logo} style={{width:300, padding:15}} alt="onlypins logo" /></p>
-        <p><a href="https://reddit.com/r/lockpicking"><img src={Redlogo} style={{width:50, height:50, padding:1}} alt="reddit link logo"/></a> <a href="https://discord.gg/5sfRmmS8"> <img src={Dislogo} style={{width:50, height:50, padding:1}} alt="discord link logo" /></a></p>
-      </div>
+        </div>
     </div>
   );
-}
+};
+
+//<p><img src={Lpu} style={{width:200, height:200, padding:15}} alt="lpu logo" /> <img src={Logo} style={{width:300, padding:15}} alt="onlypins logo" /></p>
+        //<p><a href="https://reddit.com/r/lockpicking"><img src={Redlogo} style={{width:50, height:50, padding:1}} alt="reddit link logo"/></a> <a href="https://discord.gg/5sfRmmS8"> <img src={Dislogo} style={{width:50, height:50, padding:1}} alt="discord link logo" /></a></p>
+      //</div>
